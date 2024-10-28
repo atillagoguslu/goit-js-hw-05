9a84ee3012f70a90767748bdb9d8710a22878ce2
AtillaG <atillagoguslu@hotmail.com>
2024-10-29 01:22:57 +0300

    Uc task bitti

---
+++ js/task-1.js
@@ -0,0 +1,58 @@
+// function getUserNames(users) {
+//   const newArray = [];
+//   let counter = 0;
+//   for (const user of users) {
+//     counter += 1;
+//     console.log(`UserName = ${counter}.`, user.name);
+//     newArray.push(user.name);
+//   }
+//   return newArray;
+// }
+
+function getUserNames(users) {
+  let newArray = [];
+  newArray = users.map((val, ind, arr) => {
+    return val.name;
+  });
+  return newArray;
+}
+
+console.log(
+  getUserNames([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      balance: 2811,
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      balance: 3821,
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      balance: 3793,
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      balance: 2278,
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      balance: 3951,
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      balance: 1498,
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      balance: 2764,
+    },
+  ])
+); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
